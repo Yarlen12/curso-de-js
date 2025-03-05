2d700420e0caa87c455a628a639c59dc73df3fd7
Yarlen <2020030817@upsin.edu.mx>
2025-03-04 21:21:18 -0700

    iniciamos con console log hola mundo

---
+++ src/js/00-Helloworld.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+console.log("Mi nombre es" + " " + "Yarlen");
+console.log("Hola como estan" + " " + "todos");
+
+console.log("5");
+console.log(5);
+
+console.log(5 + 3);
+console.log(5 / 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 ** 3);
+console.log(5 % 3);
+
+console.log(5 > 3);
