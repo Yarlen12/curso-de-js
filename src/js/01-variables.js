4237795784cd8b136d14becf2bd51e083749e5a4
Yarlen <2020030817@upsin.edu.mx>
2025-03-04 22:36:35 -0700

    Ejemplo de como se utiliza Let

--- src/js/01-variables.js
+++ src/js/01-variables.js
@@ -4,3 +4,23 @@ console.log(helloworld);
 
 helloworld = "Tu puedes hacerlo";
 console.log(helloworld);
+
+// Let
+
+let helloworld2 = "Hola, Javascript con let";
+console.log(helloworld2);
+
+helloworld2 = "Hola de nuevooooo";
+console.log(helloworld2);
+
+// ejemplo de let solo es un ejemplo hecho por mi
+function ejemploLet() {
+  let y;
+  let x;
+  if (true) {
+    y = 300;
+    x = 400;
+  }
+  console.log(y - x);
+}
+ejemploLet();
