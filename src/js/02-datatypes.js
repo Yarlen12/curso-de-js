fa47dd490639be64540d78e26913613bbb959611
Yarlen <2020030817@upsin.edu.mx>
2025-03-05 16:43:43 -0700

    Tipos de datos

---
+++ src/js/02-datatypes.js
@@ -0,0 +1,33 @@
+// String tipo de dato
+
+let name = "Yarlen Almogabar";
+let alias = "Yar";
+let email = `yarlen.alm@gmail.com`;
+
+console.log(name + " " + alias + " " + email);
+
+// Number tipo de dato
+let age = 22; //Entero
+let size = 1.58; //Decimal
+let phone = 6677254940;
+
+// Boolean tipo de dato
+let isStudent = true;
+let isTeacher = false;
+
+// undefined
+let undefinedValue;
+console.log(undefinedValue);
+
+// null
+let nullValue = null;
+
+// symbol
+let mySymbol = Symbol("mysymbol");
+
+// BigInt
+let myBigInt = BigInt(6837326783269873823673);
+let myBigInt2 = 843276829376376348n;
+
+// Mostramos los tipos en consola
+console.log(typeof name);
