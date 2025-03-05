d256b57fe12da5fe07fe69965123e4444b57b21f
Yarlen <2020030817@upsin.edu.mx>
2025-03-05 16:51:07 -0700

    tipos de datos en console

--- src/js/02-datatypes.js
+++ src/js/02-datatypes.js
@@ -31,3 +31,21 @@ let myBigInt2 = 843276829376376348n;
 
 // Mostramos los tipos en consola
 console.log(typeof name);
+console.log(typeof alias);
+console.log(typeof email);
+
+console.log(typeof age);
+console.log(typeof size);
+console.log(typeof phone);
+
+console.log(typeof isStudent);
+console.log(typeof isTeacher);
+
+console.log(typeof undefinedValue);
+
+console.log(typeof nullValue);
+
+console.log(typeof mySymbol);
+
+console.log(typeof myBigInt);
+console.log(typeof myBigInt2);
