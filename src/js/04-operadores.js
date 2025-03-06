2f46234006fcedd4a41eeeabcfc8830686bf634a
Yarlen <2020030817@upsin.edu.mx>
2025-03-05 17:56:23 -0700

    Operadores aritmeticos

--- src/js/04-operadores.js
+++ src/js/04-operadores.js
@@ -1,1 +1,22 @@
 // Operadores
+
+// Operadores Aritmeticos
+
+let a = 5;
+let b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log(a % b);
+console.log(a ** b);
+console.log(a > b);
+console.log(a < b);
+
+a++; //incremento
+console.log(a);
+
+b--; // Decremento
+console.log(b);
