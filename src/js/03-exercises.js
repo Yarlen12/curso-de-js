6986490dfc0b6370270117c0e5ed17d00d56914c
Yarlen <2020030817@upsin.edu.mx>
2025-03-05 17:35:04 -0700

    Ejercicios del curso, primer pregunta terminada

---
+++ src/js/03-exercises.js
@@ -0,0 +1,21 @@
+// 1. Escribe un cometario en una linea
+
+// Hola este es un comentario de una linea
+
+// 2. Escribe un comentario en varias lineas
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+// 4. Imprime por consola el valor de todas las variables
+
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuacion, modifica los valores de las variables por otros de mismo tipo
+
+// 7. A continuacion, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuacion, modifica los valores de las constantes
+
+// 10. Comenta las lineas que produzcan algun tipo de error al ejecutarse
