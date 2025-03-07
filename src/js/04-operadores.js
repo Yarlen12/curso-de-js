67c77fdc32f707f640d05da1d5e023a5bac58bac
Yarlen <2020030817@upsin.edu.mx>
2025-03-06 20:54:24 -0700

    tema operadores de comparacion termianda

--- src/js/04-operadores.js
+++ src/js/04-operadores.js
@@ -36,3 +36,28 @@ myVariable %= 2;
 myVariable **= 2;
 
 // Operadores de comparacion
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a == 6); // Igualdad por valor
+console.log(a == "6"); // Igualdad por valor
+console.log(a == a);
+console.log(a === a); // Igualdad por identidad valor y tipo
+console.log(a === 6);
+console.log(a === "6");
+console.log(a != 6); // operador de desigualdad
+console.log(a !== "6"); // desigualdad por identidad valor y tipo
+console.log(0 == false);
+console.log(1 == false);
+console.log(2 == false);
+console.log(0 == "");
+console.log(0 == " ");
+console.log(0 == "Hola");
+console.log(0 === "");
+console.log(undefined == null); // true porque los dos estan vacios
+console.log(undefined === null); // obvio da false porque son de distinto tipo
+
+// Truthy values (valores verdaderos)
