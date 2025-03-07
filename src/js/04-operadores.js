684f3d7d9bea07aefccd36479ad30b4170ab8b0c
Yarlen <2020030817@upsin.edu.mx>
2025-03-06 22:27:31 -0700

    terminamos con los temas de operadores

--- src/js/04-operadores.js
+++ src/js/04-operadores.js
@@ -61,3 +61,50 @@ console.log(undefined == null); // true porque los dos estan vacios
 console.log(undefined === null); // obvio da false porque son de distinto tipo
 
 // Truthy values (valores verdaderos)
+
+// Todos los numero positivos y negativos menos el cero
+// Todas las cadenas de texto menos las vacias
+// El boolean true
+
+// Falsy values (valores falsos)
+
+// 0
+// 0n
+// null
+// undefined
+// Nan
+// El boolean false
+// Cadenas de texto vacias
+
+// Operadores logicos
+
+// and (&&)
+console.log(5 > 10 && 15 > 20);
+console.log(5 < 10 && 15 < 20);
+console.log(5 > 10 && 15 < 20); // como es una linea si uno es falso todo es falso, es todo o nada, solo con tener un falso y lo demas verdadero ya todo sera falso
+console.log(5 < 10 && 15 > 20); // todo da falso aunque la primera condicion sea verdadera porque la segunda es falsa
+console.log(5 < 10 && 15 < 20 && 20 < 30);
+
+// or (||)
+console.log(5 > 10 || 15 > 20);
+console.log(5 < 10 || 15 < 20);
+console.log(5 < 10 || 15 > 20); // en or si tienes una condicion verdadera ya toda la linea sera true porque es 5 < 10 0 15 > 20 el primero es verdadero, es uno o otro, si los dos estan mal seria falso
+console.log(5 > 10 || 15 > 20 || 30 > 40);
+
+console.log((5 > 10 && 15 > 20) || 30 < 40); // se pueden usar combinados
+
+// not (!)
+console.log(!true);
+console.log(!false);
+console.log(!(5 > 10 && 15 > 20)); // si niego el valor, invierto su valor por lo cual me dara true
+console.log(!(5 > 10 || 15 > 20));
+
+// Operadores ternarios
+const isRaining = true;
+
+isRaining ? console.log("Esta lloviendo") : console.log("No esta lloviendo"); // El signo de interrogacion es si se cumple la condicion y los dos puntos es por si no se cumple
+
+const isRaining2 = false;
+isRaining2
+  ? console.log("Esta lloviendo")
+  : console.log("No esta lloviendo jaja");
