3a2d0b391f112c78f0190c43b93cbb5dbe168e3a
Yarlen <2020030817@upsin.edu.mx>
2025-03-10 19:32:38 -0700

    Ejercicios del tema Strings

---
+++ src/js/07-strings-exercise.js
@@ -0,0 +1,19 @@
+// 1. Concatena dos cadenas de texto
+
+// 2. Muestra la longitud de una cadena de texto
+
+// 3. Muestra el primer y el utlimo caracter de un string
+
+// 4. Convierte a mayusculas y minusculas un string
+
+// 5. Crea una cadena de texto en varias lineas
+
+// 6. Interpola el valor de una variable en un string
+
+// 7. Reempleza todos los espacion en blanco de un string por guines
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+// 9. Comprueba si dos strings son isguales
+
+// 10. Comprueba si dos strings tienes la misma longitud.
