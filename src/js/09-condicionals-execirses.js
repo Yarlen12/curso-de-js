93ba40b6cc81978362a8d0ce23ca67d0a6a3097c
Yarlen <2020030817@upsin.edu.mx>
2025-03-13 16:47:57 -0700

    Ejercicios sobre el tema de condicionals

---
+++ src/js/09-condicionals-execirses.js
@@ -0,0 +1,23 @@
+// if/else/else if/ternaria
+
+// 1. Imprime por consola tu nombre si una variable toma su valor
+
+// 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+
+// 3. Verifica si un numero es porsitivo, negativo o cero e imprime un mensaje
+
+// 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuantos años le faltan
+
+// 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable dependiendo de la edad
+
+// 6. Muestra en que estacion del año nos encontramos dependiendo del valor de una variable "mes"
+
+// 7. Muestra el numero de dias que tiene un mes dependiendo de la variable del ejercicio anterior
+
+// Switch
+
+// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+
+// 9. Usa un switch para hacer de nuevo el ejercicio 6
+
+// 10. Usa un switch para hacer de nuevo el ejercicio 7
