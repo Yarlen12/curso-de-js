19a498d896755d0e29bc70a254c28b9805e265d6
Yarlen <2020030817@upsin.edu.mx>
2025-03-13 17:02:39 -0700

    Segunda pregunta terminada

--- src/js/09-condicionals-execirses.js
+++ src/js/09-condicionals-execirses.js
@@ -8,7 +8,18 @@ if (name == "Yarlen") {
   console.log(name);
 }
 
-// 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+// 2. Imprime por consola un mensaje si el usuario y contraseña conside con unos establecidos
+
+let usuario = "Yarlen.Vazz";
+let contraseña = "Yarlen";
+
+if (usuario == "Yarlen.Vazz") {
+  console.log("Hola, usuario Yarlen.Vazz!");
+} else if (contraseña == "Yarlen") {
+  console.log("Contraseña correcta, bienvenido!");
+} else {
+  console.log("Usuario o contraseña incorrectos intente de nuevo");
+}
 
 // 3. Verifica si un numero es porsitivo, negativo o cero e imprime un mensaje
 
