398362968121de45faba4365cf9b9b6d967c9c0c
Yarlen <2020030817@upsin.edu.mx>
2025-03-15 14:53:02 -0700

    Seguimos con el tema sets

--- src/js/11-sets.js
+++ src/js/11-sets.js
@@ -1,1 +1,28 @@
 // Sets
+
+// Declaracion
+
+let mySet = new Set();
+let mySet2 = {};
+
+console.log(mySet);
+console.log(mySet2);
+
+// Inicializacion
+
+mySet = new Set([
+  "Yarlen",
+  "Almogabar",
+  "Yar",
+  22,
+  true,
+  "yarlen.alm@gmail.com",
+]); // Para que esto funcione tiene que llevar corchetes ya que solo con parentesis solo agarrara el primer valor y lo que contara son las letras
+console.log(mySet);
+
+// Metodos comunes
+
+// add y delete
+
+mySet.add("https://yarlen.dev");
+console.log(mySet);
