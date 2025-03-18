b21f90c7035ae2c622dddc05549d3103d8b580e7
Yarlen <2020030817@upsin.edu.mx>
2025-03-17 22:11:03 -0700

    Septima pregunta terminada

--- src/js/13-structure-execise.js
+++ src/js/13-structure-execise.js
@@ -38,6 +38,25 @@ console.log(mySet);
 
 // 7. Crea un mapa que asocie el numero del mes a su nombre
 
+let myMap = new Map();
+
+myMap = new Map([
+  [1, "Enero"],
+  [2, "Febrero"],
+  [3, "Marzo"],
+  [4, "Abril"],
+  [5, "Mayo"],
+  [6, "Junio"],
+  [7, "Julio"],
+  [8, "Agosto"],
+  [9, "Septiembre"],
+  [10, "Octubre"],
+  [11, "Noviembre"],
+  [12, "Diciembre"],
+]);
+
+console.log(myMap);
+
 // 8. Comprueba si el mes numero 5 existe en el map e imprime su valor
 
 // 9. Añade al mapa una clave con un array como que almacene los meses de verano
