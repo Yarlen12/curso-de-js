fde59d82845427377752d65088861b5a87e51e75
Yarlen <2020030817@upsin.edu.mx>
2025-03-17 21:12:25 -0700

    Ejecicios sobre el tema Arrays, Sets y Maps

---
+++ src/js/13-structure-execise.js
@@ -0,0 +1,19 @@
+// 1. Crea un array que almacena cinco animales
+
+// 2. Añade dos mas, uno al principio y otro al final
+
+// 3. Elimina el que se encuentra en tercera posicion
+
+// 4. Crea un set que almacene cinco libros
+
+// 5. Añade dos mas, uno de ellos repetido
+
+// 6. Elimina uno cocreto a tu eleccion
+
+// 7. Crea un mapa que asocie el numero del mes a su nombre
+
+// 8. Comprueba si el mes numero 5 existe en el map e imprime su valor
+
+// 9. Añade al mapa una clave con un array como que almacene los meses de verano
+
+// 10. Crea un Array, transformalo a un set y almacenalo en un Map
