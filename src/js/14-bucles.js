720b7e892930338642370972437aa645873b8657
Yarlen <2020030817@upsin.edu.mx>
2025-03-18 21:29:26 -0700

    Iniciamos con el tema bucles

---
+++ src/js/14-bucles.js
@@ -0,0 +1,3 @@
+// Loops o blucles = Se asocia a com si fuera una estructura de control
+
+// for, ejecutarse segun lo lanzamos, tiene una condicion qure se va a avaluar cada vez que se recorre ese bucle, mientras siga en true seguira ejecutandose, al llegar a false se detendra la ejecucion
