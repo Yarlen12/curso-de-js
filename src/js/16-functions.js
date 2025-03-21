6ce20ff9b625d7b2c392ed3d9ac90bbdd68bf05f
Yarlen <2020030817@upsin.edu.mx>
2025-03-21 16:22:24 -0700

    Tema de funciones terminado

--- src/js/16-functions.js
+++ src/js/16-functions.js
@@ -40,3 +40,87 @@ const myfunc3 = (name) => {
 const myfunc4 = (name) => console.log(`Hola!, ${name}!`); // No se necesitan los corchetes ya que se les designo una variable
 myfunc4("hooaoajoajaoa");
 myfunc3("Yarleeeeeeeeeeeeen");
+
+// Parametros
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(5, 3); // De esta forma le pasaremos los parametros, con comas ya que arriba ya se estan sumando, aqui nomas asignamos valor
+
+// Asignarles un valor por default
+
+const defaultSum = function (a = 0, b = 0) {
+  // Moure no necesito declarar la variable, pero yo si lo tuve que hacer y es asi
+  console.log(a + b);
+};
+
+// Por defecto
+
+defaultSum();
+defaultSum(5); // Dara 5 en consola solamente
+defaultSum(5, 3); // es por default arriba pero igual se pueden seguir modificando los valores
+// defaultSum((a = 5)); Esto no se puede hacer aparentemente no da error en codigo, pero en consola si
+
+// Retorno de valores
+
+const mult = function (a, b) {
+  return a * b; // retornarle el resultado al usuario no a consolas
+};
+
+console.log(mult(5, 10));
+
+//  es igual a
+
+let resultado = mult(10, 10);
+console.log(resultado);
+
+// Funciones anidadas
+
+function extern() {
+  console.log("Funcion externa");
+  function intern() {
+    console.log("Funcion interna");
+  }
+  intern(); // Solo de esta manera se puede, ya que se encuentra dentro
+}
+
+extern(); // Es importante realizar este paso porque si no lo escribirmos en consola no aparecera nada
+// intern(); Error: fuera del scope
+
+// Funciones de orden superior
+function applyFunc(func, param) {
+  func(param);
+}
+
+applyFunc(myfunc4, "Funcion de orden superior"); // Una funcion que recibe otra funcion y tiene un parametro para ejecutarlo
+
+// forEach: es una funcion que nos sirve para ejecutar bucles asociados a elementos iterables
+// El forEach no se puede detener con el break o continue, no retornamos un nuevo array
+let myArray = [1, 2, 3, 4];
+
+let mySet = new Set([
+  "Yarlen",
+  "Almogabar",
+  "Yar",
+  22,
+  true,
+  "yarlen.alm@gmail.com",
+]);
+
+let myMap = new Map([
+  ["name", "Yarlen"],
+  ["email", "yarlen.alm@gmail.com"],
+  ["age", 37],
+]);
+myArray.forEach(function (value) {
+  // de manera mas larga, como ya tiene asignado un valor definido, ya tendra un valor en consola, sin necesidad de llamar mas abajo a la varible ()
+  console.log(value);
+});
+
+myArray.forEach((value) => console.log(value)); // De manera mas rapida
+
+mySet.forEach((value) => console.log(value));
+
+myMap.forEach((value) => console.log(value)); // Cada calor estara en consola, cada valor sera una linea
