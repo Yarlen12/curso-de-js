c346ca02b33a4699d9713e866bb1c261c3158850
Yarlen <2020030817@upsin.edu.mx>
2025-03-21 16:23:35 -0700

    Ejecicios sobre el tema de funciones

---
+++ src/js/17-functions-exercises.js
@@ -0,0 +1,21 @@
+// NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios
+
+// 1. Crea una funcion que reciba dos numeros y devuelva su suma
+
+// 2. Crea una funcion que reciba un array de numeros y devuelva el mayor de ellos
+
+// 3. Crea una funcion que reciba un string y devuelva el numero de vocales que contiene
+
+// 4. Crea una funcion que reciba un array de strings y devuelva un nuevo array con las strings en mayusculas
+
+// 5. Crea una funcion que reciba un numero y devuelva true si es prime, y false en caso contrario
+
+// 6. Crea una funcion que reciba dos arryas y devuelva un nuevo array que contenga los elementos comunes entre ambos
+
+// 7. Crea una funcion que reciba un array de numeros y devuelva la suma de todos los numeros pares
+
+// 8. Crea una funcion que reciba un array de numeros y devuekva un nuevo array con cada numero elevado al cuadrado
+
+// 9. Crea una funcion que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden
+
+// 10. Crea una funcion que calcule el factorial de un numero dados
