844213d0d91c276a0c9e4e2d11df1bec16c3be60
Yarlen <2020030817@upsin.edu.mx>
2025-03-25 14:46:54 -0700

    Ejercicios sobre el tema de desestructuracion y propagacion

---
+++ src/js/21-destructuring-spreading-exercises.js
@@ -0,0 +1,19 @@
+// 1. Usa desestructuracion para extraer los dos primeros elementos de un array
+
+// 2. Usa desestructuracion en un array y asigna un valor predeterminado a una variable
+
+// 3. Usa desestructuracion para extraer dos propiedades de un objeto
+
+// 4. Usa desestructuracion para extraer dos propiedades de un objeto y asignalas a nuevas variables con nombres diferentes
+
+// 5. Usa desestructuracion para extraer dos propiedades de un objeto anidado
+
+// 6. Usa propagacion para combinar dos arrays en uno nuevo
+
+// 7. Usa propagacion para crear una copia de un array
+
+// 8. Usa propagacion para combinar dos objetos en uno nuevo
+
+// 9. Usa propagacion para crear una copia de un objeto
+
+// 10. Combina desestructuracion y propagacion
