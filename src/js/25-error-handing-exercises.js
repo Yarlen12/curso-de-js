73c552fb1d1667d02205fd1d3b9a87df73a77098
Yarlen <2020030817@upsin.edu.mx>
2025-04-01 14:33:24 -0700

    Ejercicios del tema de manejo de errores

---
+++ src/js/25-error-handing-exercises.js
@@ -0,0 +1,19 @@
+// 1. Captura una excepcion utilizando try-catch
+
+// 2. Capura una excepcion utilizando try-catch y finally
+
+// 3. Lanza una excepcion generica
+
+// 4. Crea una excepcion personalizada
+
+// 5. Lanza una excepcion personalizada
+
+// 6. Lanza varias excepciones segun una logica definida
+
+// 7. Captura varias excepciones en un mismo try-catch
+
+// 8. Crea un bucle que intente transformar a float cada valor y capture y muestre los errores
+
+// 9. Crea una funcion que verifique si un objeto tiene una propiedad especifica y lanza una excepcion personal
+
+// 10. Crea una funcion que realice reintentos en caso de error hasta un maximo de 10
