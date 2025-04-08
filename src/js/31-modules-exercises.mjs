ee69c6d6409bd1cdf67e841ea42c3a68ee300d83
Yarlen <2020030817@upsin.edu.mx>
2025-04-08 16:58:23 -0700

    Tercera pregunta terminada

--- src/js/31-modules-exercises.mjs
+++ src/js/31-modules-exercises.mjs
@@ -12,6 +12,17 @@ export const name = "Yarlen";
 
 // 3. Exporta una clase
 
+export class Nombres {
+  constructor(name1, name2) {
+    this.name1 = name1;
+    this.name2 = name2;
+  }
+
+  saludo() {
+    console.log(`Hola ${this.name1} y ${this.name2} mucho gusto en conocerlos`);
+  }
+}
+
 // 4. Importa una funcion
 
 // 5. Importa una constante
