8588f5c3d5de3dafafe6e1e98a32c315ff9764f2
Yarlen <2020030817@upsin.edu.mx>
2025-04-07 17:33:59 -0700

    cambios

--- src/js/28-export-modules.js
+++ src/js/28-export-modules.js
@@ -1,1 +1,7 @@
 // Exportacion de modulos: Es una forma de dividir nuestro codigo en piezas que nos permite organizar nuestro codigo en archivos separados
+
+function add(a, b) {
+  return a + b;
+}
+
+add(5, 10);
