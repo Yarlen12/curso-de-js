8952f4144c53464a6d9157b301cb0c3e39b20848
Yarlen <2020030817@upsin.edu.mx>
2025-04-07 22:18:18 -0700

    Tema terminado del import module

--- src/js/28-export-modules.js
+++
@@ -1,7 +0,0 @@
-// Exportacion de modulos: Es una forma de dividir nuestro codigo en piezas que nos permite organizar nuestro codigo en archivos separados
-
-function add(a, b) {
-  return a + b;
-}
-
-add(5, 10);
