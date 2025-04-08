cc76a507aade2456b2d6a614d1304e84dc5e8af8
Yarlen <2020030817@upsin.edu.mx>
2025-04-08 16:48:54 -0700

    Primera pregunta terminada

---
+++ src/js/31-modules-exercises.mjs
@@ -0,0 +1,25 @@
+// 1. Exporta una funcion
+
+export function suma(a, b) {
+  return a + b;
+}
+
+console.log(suma(5, 10));
+
+// 2. Exportan una constante
+
+// 3. Exporta una clase
+
+// 4. Importa una funcion
+
+// 5. Importa una constante
+
+// 6. Importa una clase
+
+// 7. Exporta una funcion, una constante y una clase por defecto
+
+// 8. Importa una funcion, una constante y una clase por defecto
+
+// 9. Exporta una funcion, una constante y una clase desde una carpeta
+
+// 10. Importa una funcion, una constante y una clase desde un directorio diferente al anterior
