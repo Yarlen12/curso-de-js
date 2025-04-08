5d96152e28eefe39f810dac5e28c5a119c67878c
Yarlen <2020030817@upsin.edu.mx>
2025-04-07 22:21:04 -0700

    modulos importados externos tema terminado

---
+++ src/js/30-import-external-modules.cjs
@@ -0,0 +1,8 @@
+// Modulos externos
+
+const os = require("os");
+
+console.log(os.platform());
+console.log(os.arch()); // arquitectura
+console.log(os.totalmem()); // Memoria total
+console.log(os.freemem()); // Memoria libre
