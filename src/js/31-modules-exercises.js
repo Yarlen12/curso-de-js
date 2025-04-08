abb35f991dde59dd34aa586ae91450eb744752f2
Yarlen <2020030817@upsin.edu.mx>
2025-04-07 22:25:32 -0700

    Ejercicios sobre el tema de modulos

---
+++ src/js/31-modules-exercises.js
@@ -0,0 +1,19 @@
+// 1. Exporta una funcion
+
+// 2. Exportan una constante
+
+// 3. Exporta una clase
+
+// 4. Importa una funcion
+
+// 5. Importa una constante
+
+// 6. Importa una clase
+
+// 7. Exporta una funcion, una constante y una clase por defecto
+
+// 8. Importa una funcion, una constante y una clase por defecto
+
+// 9. Exporta una funcion, una constante y una clase desde una carpeta
+
+// 10. Importa una funcion, una constante y una clase desde un directorio diferente al anterior
